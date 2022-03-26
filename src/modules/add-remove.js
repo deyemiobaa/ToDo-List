a0d40e387f6d759616ae0c4adaf209ae8a7a6fe6
Sodiq Aderibigbe <sodiqaderibigbe90@gmail.com>
2022-03-26 11:32:14 +0100

    Make input field interactive

--- src/modules/add-remove.js
+++ src/modules/add-remove.js
@@ -1,6 +1,6 @@
 import NewTask from './createTask.js';
 import {
-  markItemAsComplete, deleteAllTasks,
+  markItemAsComplete, deleteAllTasks, updateInput, makeInputDefault
 } from './interactive-page.js';
 
 const taskContainer = document.querySelector('.task-container');
@@ -8,7 +8,6 @@ const addNewTaskInput = document.getElementById('add-new-task');
 const enterBtn = document.querySelector('.enter');
 const clearAllBtn = document.getElementById('clear-btn');
 const refresh = document.querySelector('.refresh');
-
 class AddRemoveTask {
   constructor() {
     this.store = JSON.parse(localStorage.getItem('ToDoList')) || [];
@@ -20,6 +19,7 @@ class AddRemoveTask {
       task.index = initialIndex;
       initialIndex += 1;
     });
+    localStorage.setItem('ToDoList', JSON.stringify(this.store));
   }
 
   clearCompletedTasks(button) {
@@ -36,7 +36,7 @@ class AddRemoveTask {
   deleteTask(button) {
     button.addEventListener('click', () => {
       this.store = this.store.filter(
-        (tasks) => button.id !== tasks.id,
+        (tasks) => button.parentElement.id !== tasks.id,
       );
       taskContainer.innerHTML = '';
       this.store.forEach((task) => {
@@ -52,11 +52,15 @@ class AddRemoveTask {
       <div class="task">
           <input type="checkbox" class="check">
           <input id="${id}" class="new-task" type="text" value="${task}">
-          <span id="${id}" class="delete">
-            <i class="fa-solid fa-trash-can"></i>
+          <span id="${id}">
+            <i class="delete fa-solid fa-trash-can"></i>
           </span>
       </div>
       `;
+
+    // mark item as completed
+    const checkBox = document.querySelectorAll('.check');
+    markItemAsComplete(checkBox, this.store);
 
     const deleteBtn = document.querySelectorAll('.delete');
     deleteBtn.forEach((btn) => {
@@ -65,9 +69,16 @@ class AddRemoveTask {
     });
     localStorage.setItem('ToDoList', JSON.stringify(this.store));
 
-    // mark item as completed
-    const checkBox = document.querySelectorAll('.check');
-    markItemAsComplete(checkBox, this.store);
+    // edit an input
+    const fieldInputs = document.querySelectorAll('.new-task')
+    fieldInputs.forEach(field => {
+      field.addEventListener('click', () => {
+        updateInput(field, this.store)
+      })
+      field.addEventListener('blur', () => {
+        makeInputDefault(field)
+      })
+    })
   }
 
   localStorageToWebpage() {
@@ -99,7 +110,6 @@ class AddRemoveTask {
         this.addNewTask();
       }
     });
-
     this.clearCompletedTasks(clearAllBtn);
     localStorage.setItem('ToDoList', JSON.stringify(this.store));
     this.localStorageToWebpage();
